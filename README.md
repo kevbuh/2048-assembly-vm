fce7900cb9ae0deb0f20c468fd1835ae0d095c66
Kevin Buhler <buhlerkw@gmail.com>
2025-01-09 11:55:20 -0700

    executing programs outline

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 emulates https://en.wikipedia.org/wiki/Little_Computer_3
 
 https://www.jmeiners.com/lc3-vm/#:memory-mapped-registers
+
+### LC-3 Assembly Hello World
+
+```
+.ORIG x3000                        ; memory address where program will be loaded
+LEA R0, HELLO_STR                  ; load address of HELLO_STR string into R0
+PUTs                               ; output string pointed to by R0 to console
+HALT                               ; halt program
+HELLO_STR .STRINGZ "Hello World!"  ; store string here in the program
+.END                               ; mark end of the file
+```
--- main.c
+++ main.c
@@ -67,3 +67,126 @@ enum
     FL_ZRO = 1 << 1, /* Z */
     FL_NEG = 1 << 2, /* N */
 };
+
+// INPUT BUFFERING
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+int main(int argc, const char* argv[])
+{
+    // LOAD ARGS
+    if (argc < 2) {
+        // show usage string
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j) {
+        if (!read_image(argv[j])) {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    // SETUP
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
+    // since exactly one condition flag should be set at any given time, set the Z flag
+    reg[R_COND] = FL_ZRO;
+
+    // set the PC to starting position
+    // 0x3000 is the default
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+        switch (op)
+        {
+            case OP_ADD:
+                @{ADD}
+                break;
+            case OP_AND:
+                @{AND}
+                break;
+            case OP_NOT:
+                @{NOT}
+                break;
+            case OP_BR:
+                @{BR}
+                break;
+            case OP_JMP:
+                @{JMP}
+                break;
+            case OP_JSR:
+                @{JSR}
+                break;
+            case OP_LD:
+                @{LD}
+                break;
+            case OP_LDI:
+                @{LDI}
+                break;
+            case OP_LDR:
+                @{LDR}
+                break;
+            case OP_LEA:
+                @{LEA}
+                break;
+            case OP_ST:
+                @{ST}
+                break;
+            case OP_STI:
+                @{STI}
+                break;
+            case OP_STR:
+                @{STR}
+                break;
+            case OP_TRAP:
+                @{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                @{BAD OPCODE}
+                break;
+        }
+    }
+
+    restore_input_buffering();
+}
