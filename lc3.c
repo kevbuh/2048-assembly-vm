9f6ded95fc1849d3fa8e94a1aa70b78dcc135209
Kevin Buhler <buhlerkw@gmail.com>
2025-01-09 14:29:28 -0700

    TRAP routines

--- lc3.c
+++ lc3.c
@@ -10,10 +10,19 @@
 #include <sys/termios.h>
 #include <sys/mman.h>
 
-
-#define MEMORY_MAX (1 << 16)
+// TRAP CODES
+enum
+{
+    TRAP_GETC = 0x20,  // get character from keyboard, not echoed onto the terminal
+    TRAP_OUT = 0x21,   // output a character
+    TRAP_PUTS = 0x22,  // output a word string
+    TRAP_IN = 0x23,    // get character from keyboard, echoed onto the terminal
+    TRAP_PUTSP = 0x24, // output a byte string
+    TRAP_HALT = 0x25   // halt the program
+};
 
 // MEMORY ARRAY
+#define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];  // 65536 mem locations
 
 // REGISTERS
@@ -154,7 +163,7 @@ int main(int argc, const char* argv[])
 
     // set the PC to starting position
     // 0x3000 is the default
-    enum { PC_START = 0x3000 };
+    enum { PC_START = 0x3000 }; // lower addresses are left empty to leave space for the trap routine code
     reg[R_PC] = PC_START;
 
     // LOOP
@@ -304,8 +313,69 @@ int main(int argc, const char* argv[])
                     break;
                 }
             case OP_TRAP:
-                @{TRAP}
-                break;
+                {
+                    reg[R_R7] = reg[R_PC];
+
+                    switch (instr & 0xFF)
+                    {
+                        case TRAP_GETC: // read a single ASCII char
+                            {
+                                reg[R_R0] = (uint16_t)getchar();
+                                update_flags(R_R0);
+                                break;
+                            }
+                        case TRAP_OUT: // output a character
+                            {
+                                putc((char)reg[R_R0], stdout);
+                                fflush(stdout);
+                                break;
+                            }
+                        case TRAP_PUTS: // output a null terminated string
+                            {
+                                uint16_t* c = memory + reg[R_R0];
+                                while (*c) {
+                                    putc((char)*c, stdout);
+                                    ++c;
+                                }
+                                fflush(stdout);
+                                break;
+                            }
+                        case TRAP_IN: // input character
+                            {
+                                printf("Enter a character: ");
+                                char c = getchar();
+                                putc(c, stdout);
+                                fflush(stdout);
+                                reg[R_R0] = (uint16_t)c;
+                                update_flags(R_R0);
+                                break;
+                            }
+                        case TRAP_PUTSP: // output string
+                            {
+                                /* one char per byte (two bytes per word)
+                                here we need to swap back to big endian format */
+                                uint16_t* c = memory + reg[R_R0];
+                                while (*c)
+                                {
+                                    char char1 = (*c) & 0xFF;
+                                    putc(char1, stdout);
+                                    char char2 = (*c) >> 8;
+                                    if (char2) putc(char2, stdout);
+                                    ++c;
+                                }
+                                fflush(stdout);
+                                break;
+                            }
+                        case TRAP_HALT: // halt program
+                            {
+                                puts("HALT");
+                                fflush(stdout);
+                                running = 0;
+                                break;
+                            }
+                    }
+                    break;
+                }
             case OP_RES:
             case OP_RTI:
             default:
